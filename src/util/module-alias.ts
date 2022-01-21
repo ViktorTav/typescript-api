356fc782278dc42e5df3e6ec04b60ac1524cb583
ViktorTav <viktortaveira@gmail.com>
2022-01-21 10:04:18 -0300

    first commit

---
+++ src/util/module-alias.ts
@@ -0,0 +1,15 @@
+import * as path from "path"
+import moduleAlias from "module-alias"
+
+//Importa todos os arquivos da pasta raiz do projeto.
+const files = path.resolve(__dirname,"../..");
+
+/*
+    Usamos o moduleAlias no projeto para que depois que o código do mesmo
+    seja compilado, não termos erros por estarmos usando os paths (@src,@test)
+    que foram configurados em tsconfig.json.
+*/
+moduleAlias.addAliases({
+    "@src":path.join(files,'src'),
+    "@test": path.join(files,'test')
+})
